1694ee79396612063610a6c1bf7a9bd8f0816486
Yerson <yersoncrespo@hotmail.com>
2018-10-28 19:21:34 -0500

    2st

--- index.js
+++ index.js
@@ -1,49 +1,35 @@
-/*
-In the node.js intro tutorial (http://nodejs.org/), they show a basic tcp 
-server, but for some reason omit a client connecting to it.  I added an 
-example at the bottom.
 
-Save the following server in example.js:
-*/
+// https://millermedeiros.github.io/mdoc/examples/node_api/doc/net.html#net.createServer
+// telenet 127.0.0.1 8080
 
 var net = require('net');
+const fs = require('fs');
 
-var server = net.createServer(function(socket) {
-	socket.write('Echo server\r\n');
-	socket.pipe(socket);
+var server = net.createServer(function(socket) { //'connection' listener
+    
+    console.log('Server started: Waiting for client connection ...');
+    console.log('Client connected:port,address: '+socket.remotePort,      socket.remoteAddress);
+
+    console.log(socket);
+
+    socket.on('data', function(data){
+        var date = new Date();
+        var today = date.getDate()+'_'+date.getMonth();
+        fs.appendFile(today+'_log.txt', data, function (err) {
+            if (err) throw err;
+            console.log(data.toString())
+            //console.log(data.toString('ascii'))
+        });
+    });
+
+    socket.on('end', function() {
+        console.log('server disconnected');
+    });
+
+    socket.write('Hello cliet, this is IPO Avaya Bridge Server.\r\n');
+    socket.pipe(socket);
 });
 
-server.listen(8080, '127.0.0.1');
-
-/*
-And connect with a tcp client from the command line using netcat, the *nix 
-utility for reading and writing across tcp/udp network connections.  I've only 
-used it for debugging myself.
-
-$ netcat 127.0.0.1 1337
-
-You should see:
-> Echo server
-
-*/
-
-/* Or use this example tcp client written in node.js.  (Originated with 
-example code from 
-http://www.hacksparrow.com/tcp-socket-programming-in-node-js.html.) */
-
-var net = require('net');
-
-var client = new net.Socket();
-client.connect(8080, '127.0.0.1', function() {
-	console.log('Connected');
-	client.write('Hello, server! Love, Client.');
-});
-
-client.on('data', function(data) {
-	console.log('Received: ' + data);
-	client.destroy(); // kill client after server's response
-});
-
-client.on('close', function() {
-	console.log('Connection closed');
+server.listen(8080, function() { //'listening' listener
+  console.log('server bound');
 });
